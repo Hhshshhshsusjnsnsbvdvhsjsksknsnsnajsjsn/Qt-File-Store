252001afd2089164f4e7d2861371c74bbaa27a18
Ksjsjjsksksisjsnsnsnzzbzysyshshshjsjsj <koushalprasad35@gmail.com>
2024-11-12 12:31:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -70,14 +70,14 @@ WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or Fal
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://qt-movie.blogspot.com/2024/08/qt-movie-bot.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = False
+    ON_HEROKU = True
 else:
     ON_HEROKU = True
 URL = environ.get("URL", "")
